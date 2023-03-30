e0d4cd006b817307542e905100f6282e817d67bd
lucasbeluati <126527842+lucasbeluati@users.noreply.github.com>
2023-03-30 11:31:32 -0300

    initial commit

---
+++ CVjs.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').addEventListener('click', function(){
+
+    document.getElementById('info').style.display='block';
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# CV_Lucas_Beluati
+Repo con mi proyecto de CV.
